a2a260250964df86fe6b3a16b062e53a26f18a16
Myron Dcunha <67193041+Myron2812@users.noreply.github.com>
2024-01-02 15:10:24 +0530

    Delete nvim directory

--- nvim/lua/core/options.lua
+++
@@ -1,7 +0,0 @@
-local vim = vim
-local opt = vim.opt
-
-vim.wo.number = true
-opt.foldmethod = "expr"
-opt.foldexpr = "nvim_treesitter#foldexpr()"
-
--- nvim/lua/core/plugin_config/navigator.lua
+++
@@ -1,9 +0,0 @@
-require'navigator'.setup({
-  mason = true,
-  icons = {
-    icons = true,
-    diagnostic_err = '🚫', --error symbol
-    diagnostic_warn = '⚠️', --warning symbol
-    diagnostic_virtual_text = '💡'
-  }
-})
